a19053e4042b121183ea095d8630dcf0fc7db02e
eliaselherrera96 <eliaselherrera90@gmail.com>
2023-06-25 20:36:48 +0200

    new commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <p className='text-sm py-2'> &copy; 2023 Elias El Herrera All rights reserved</p>
+    <p className='text-sm py-2 flex justify-center'> &copy; 2023 Elias El Herrera All rights reserved</p>
   )
 }
 
